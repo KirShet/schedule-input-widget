484925b617a6bed38edbb630383092a1f2846e9e
KirShet <kirshet2000@gmail.com>
2024-12-19 11:43:31 +0600

    Update

--- src/widgets/ScheduleInputWidget/views/scheduleInputWidget.php
+++ src/widgets/ScheduleInputWidget/views/scheduleInputWidget.php
@@ -98,35 +98,37 @@
             <h2 id="modal-title">Добавление особенного дня</h2>
             <p id="modal-description">Выберите день или период, когда режим работы не совпадает с рабочим днем или дополнительным интервалом.</p>
             <section class="calendar">
+            <div class="icon-arrow arrow_back"></div>
                 <article class="month">
-                    <h3>Ноябрь 2024</h3>
-                    <div class="weekdays"><div>Пн</div><div>Вт</div><div>Ср</div><div>Чт</div><div>Пт</div><div>Сб</div><div>Вс</div></div>
-                    <div class="days">
-                        <div></div><div></div><div></div><div></div><div>1</div><div>2</div><div>3</div>
-                        <div>4</div><div>5</div><div>6</div><div>7</div>
-                        <div>8</div><div>9</div><div>10</div>
-                        <div>11</div><div>12</div><div>13</div><div>14</div>
-                        <div>15</div><div>16</div><div>17</div>
-                        <div>18</div><div>19</div><div>20</div><div>21</div>
-                        <div>22</div><div>23</div><div>24</div>
-                        <div>25</div><div>26</div><div>27</div><div>28</div>
-                        <div>29</div><div>30</div>
-                    </div>
-                </article>
-                <article class="month">
-                    <h3>Декабрь 2024</h3>
-                    <div class="weekdays"><div>Пн</div><div>Вт</div><div>Ср</div><div>Чт</div><div>Пт</div><div>Сб</div><div>Вс</div></div>
-                    <div class="days">
-                        <div></div><div></div><div></div><div></div><div></div><div></div><div>1</div><div>2</div><div>3</div><div>4</div>
-                        <div>5</div><div>6</div><div>7</div><div>8</div>
-                        <div>9</div><div>10</div><div>11</div><div>12</div>
-                        <div>13</div><div>14</div><div>15</div><div>16</div>
-                        <div>17</div><div>18</div><div>19</div><div>20</div>
-                        <div>21</div><div>22</div><div>23</div><div>24</div>
-                        <div>25</div><div>26</div><div>27</div><div>28</div>
-                        <div>29</div><div>30</div><div>31</div>
-                    </div>
-                </article>
+                    <div class="month-name">Ноябрь 2024</div>
+                        <div class="weekdays"><div>Пн</div><div>Вт</div><div>Ср</div><div>Чт</div><div>Пт</div><div>Сб</div><div>Вс</div></div>
+                        <div class="days">
+                            <div></div><div></div><div></div><div></div><div>1</div><div>2</div><div>3</div>
+                            <div>4</div><div>5</div><div>6</div><div>7</div>
+                            <div>8</div><div>9</div><div>10</div>
+                            <div>11</div><div>12</div><div>13</div><div>14</div>
+                            <div>15</div><div>16</div><div>17</div>
+                            <div>18</div><div>19</div><div>20</div><div>21</div>
+                            <div>22</div><div>23</div><div>24</div>
+                            <div>25</div><div>26</div><div>27</div><div>28</div>
+                            <div>29</div><div>30</div>
+                        </div>
+                    </article>
+                    <article class="month">
+                    <div class="month-name">Декабрь 2024</div>
+                        <div class="weekdays"><div>Пн</div><div>Вт</div><div>Ср</div><div>Чт</div><div>Пт</div><div>Сб</div><div>Вс</div></div>
+                        <div class="days">
+                            <div></div><div></div><div></div><div></div><div></div><div></div><div>1</div><div>2</div><div>3</div><div>4</div>
+                            <div>5</div><div>6</div><div>7</div><div>8</div>
+                            <div>9</div><div>10</div><div>11</div><div>12</div>
+                            <div>13</div><div>14</div><div>15</div><div>16</div>
+                            <div>17</div><div>18</div><div>19</div><div>20</div>
+                            <div>21</div><div>22</div><div>23</div><div>24</div>
+                            <div>25</div><div>26</div><div>27</div><div>28</div>
+                            <div>29</div><div>30</div><div>31</div>
+                        </div>
+                    </article>
+                <div class="icon-arrow arrow_forward"></div>
             </section>
             <section class="time-selection-wrapper">
                 <span id="selected-date">10 декабря</span> с
@@ -134,6 +136,7 @@
                 <span>до</span>
                 <input type="time" value="19:00">
             </section>
+            <div class="divider"></div>
             <div class="buttons">
                 <button class="add-btn">Добавить</button>
                 <button class="cancel-btn">Отменить</button>
